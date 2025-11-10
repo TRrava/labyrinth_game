92c936467006942d7b419fbefd6b379e3a9d8a1b
TRrava <oleg-nikolashin@mail.ru>
2025-11-10 16:48:18 +0300

    Добавлены основные файлы py. Описан файл constant.py

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -2,3 +2,13 @@
 
 def main():
     print("Welcome to Labyrinth Game")
+
+from constant import ROOMS
+
+game_state = {
+    'player_inventory': [],  # Инвентарь игрока
+    'current_room': 'entrance',  # Текущая комната
+    'game_over': False,  # Значения окончания игры
+    'steps_taken': 0  # Количество шагов
+}
+
